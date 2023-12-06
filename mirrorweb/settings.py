78f7ab801334e9d9f0463cecb9ffe486c75a19b8
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-12-06 01:08:49 +0100

    a

--- mirrorweb/settings.py
+++ mirrorweb/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app',
-                 '.now.sh', 'smartmirrorgrupo5.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'smartmirrorresidencia.azurewebsites.net']
 
 
 # Application definition
